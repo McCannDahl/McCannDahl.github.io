5e36a8926a04ededadea19071d2a055b6e5252f3
McCann Dahl <mccann.dahl@parivedasolutions.com>
2022-01-26 10:45:23 -0600

    updates

--- mccannrts/web/flutter_service_worker.js
+++ mccannrts/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "a3646ac26c3909afc28444dea74859cf",
-"index.html": "a2b1c6d91704642211008255f7b08b85",
-"/": "a2b1c6d91704642211008255f7b08b85",
-"main.dart.js": "d684646bb57671d21445ea47869ed8b0",
+  "version.json": "7a10610e87c564356c2f27db3ff532ca",
+"index.html": "9d8f508e5241541850f8bd6e3cf8b73b",
+"/": "9d8f508e5241541850f8bd6e3cf8b73b",
+"main.dart.js": "cbc6375f026794fd7c1259749a72e402",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
