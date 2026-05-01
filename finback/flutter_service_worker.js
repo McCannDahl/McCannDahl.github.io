d6cfc4403d0986114190adc39e36d86650c97f40
nomrom <mccdahl@gmail.com>
2026-04-30 23:36:31 -0500

    updated finback

--- finback/flutter_service_worker.js
+++ finback/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5f8e6588fca90bd664d2a0d3cb747b5b
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "2e413098f1af50e2d9f29dcf77f34e54",
+"flutter_bootstrap.js": "812bc62c4d35da21b2443a11ee4e7f8f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
