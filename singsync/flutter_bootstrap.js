8cbde6a171c5d18f75f15a055689d24b67f3523a
nomrom <mccdahl@gmail.com>
2026-08-04 00:37:29 -0500

    minor update

--- singsync/flutter_bootstrap.js
+++ singsync/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"db373eb85a06eb3d706af362a45d42d0972f4b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2302497948"
+    serviceWorkerVersion: "4125003138"
   }
 });
