8b9d7e17c485b2d63d70aed32db35f1f7a43182b
nomrom <mccdahl@gmail.com>
2026-08-03 22:08:31 -0500

    minor updates

--- singsync/flutter_service_worker.js
+++ singsync/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "152b743db93875f5d305de7db5ca2801
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "476682a4bc707b2061d96746befc567f",
+"flutter_bootstrap.js": "b2533fc6fba4be90eeca12f6c15717e8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
